ffd5c5585a0407db5c3e5deecdc7479c0af2676a
BigDeep <feng865380316@qq.com>
2021-08-17 10:32:59 +0800

    添加index入口文件备注

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ export default {
   createNamespacedHelpers,
   createLogger
 }
-
+// 导出了 install 函数
+// 导出了 Store 本体
+// 导出了 4个常用的解构方法。mapState、mapMutations、mapGetters、mapActions
 export {
   Store,
   install,
